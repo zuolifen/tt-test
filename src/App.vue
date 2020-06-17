4fb2db7ce746aa6fdc9b5ff9d07ef7e40e7088ea
zuolifen <512481545@qq.com>
2020-06-17 11:32:35 +0800

    init add

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <span  >{{message}}</span>
+    <button @click="reverseMsg">点击切换</button>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  data:function(){
+    return {
+      message:'hello,你好'
+    }
+  },
+  components: {
+  },
+  methods:{
+    reverseMsg:function(){
+      this.message = this.message.split('').reverse().join('')
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
